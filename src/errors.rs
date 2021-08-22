8259893e17329796c47a85bd7eb3feeb5385defa
fewensa <fewensa@protonmail.com>
2021-08-22 23:33:14 +0800

    New feature

--- src/errors.rs
+++ src/errors.rs
@@ -5,18 +5,18 @@ use std::{io, fmt, error};
 pub enum RTDError {
   Io(io::Error),
   SerdeJson(serde_json::Error),
-  Custom(&'static str),
+  Custom(String),
 }
 
 pub type RTDResult<T> = Result<T, RTDError>;
 
 impl RTDError {
-  pub fn custom(msg: &'static str) -> Self { RTDError::Custom(msg) }
+  pub fn custom(msg: String) -> Self { RTDError::Custom(msg) }
 }
 
 impl fmt::Display for RTDError {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match *self {
+    match self {
       RTDError::Io(ref err) => write!(f, "IO error: {}", err),
       RTDError::SerdeJson(ref err) => write!(f, "Serde json error: {}", err),
       RTDError::Custom(msg) => write!(f, "{}", msg),
@@ -25,14 +25,6 @@ impl fmt::Display for RTDError {
 }
 
 impl error::Error for RTDError {
-  fn description(&self) -> &str {
-    match *self {
-      RTDError::Io(ref err) => err.description(),
-      RTDError::SerdeJson(ref err) => err.description(),
-      RTDError::Custom(msg) => msg,
-    }
-  }
-
   fn cause(&self) -> Option<&dyn error::Error> {
     match *self {
       RTDError::Io(ref err) => Some(err),
@@ -53,7 +45,5 @@ impl From<serde_json::Error> for RTDError {
     RTDError::SerdeJson(err)
   }
 }
-
-//impl From<str> for
 
 
